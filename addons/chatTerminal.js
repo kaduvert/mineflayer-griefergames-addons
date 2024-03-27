1eeb8e492fc8b3a2b75b13549af467b0034f2538
KadaverBrutalo10 <jimmybiel123@gmail.com>
2024-03-27 21:18:08 +0100

    publish

--- addons/chatTerminal.js
+++ addons/chatTerminal.js
@@ -1,7 +1,7 @@
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process')
 
-module.exports = function inject(bot, options) {
+module.exports = function inject(bot) {
 	const rl = readline.createInterface({ input, output })
 	rl.on('line', bot.chat.send)
 }
