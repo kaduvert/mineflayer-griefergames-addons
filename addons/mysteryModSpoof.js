a65efc1535984a676dcdd9f56c70182303000765
KadaverBrutalo10 <jimmybiel123@gmail.com>
2024-03-27 20:18:23 +0100

    fix mysterymod

--- addons/mysteryModSpoof.js
+++ addons/mysteryModSpoof.js
@@ -1,3 +1,3 @@
 module.exports = function inject(bot, options) {
-    bot.on('misc:mysteryMod->mysterymod_user_check', bot.mysteryMod.sendPayload)
+    bot.on('misc:mysteryMod->mysterymod_user_check', bot.gg.mysteryMod.sendPayload)
 }
